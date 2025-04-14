5e49802bc024d9227b40958045bee3a6604a68ac
acmepjz <acme_pjz@hotmail.com>
2025-04-15 02:49:08 +0800

    add characteristic ideal (WIP)

--- Iwasawalib.lean
+++ Iwasawalib.lean
@@ -5,4 +5,5 @@ import Iwasawalib.Algebra.Exact.KerCokerComp
 import Iwasawalib.FieldTheory.ZpExtension.Basic
 import Iwasawalib.NumberTheory.ZpExtension.Basic
 import Iwasawalib.NumberTheory.ZpExtension.ClassGroup
+import Iwasawalib.RingTheory.CharacteristicIdeal.Basic
 import Iwasawalib.RingTheory.PseudoNull.Basic
